19343699427fbae6e8b3983a867d1cb0a6955670
iamdurga <pokhareldurga88@gmail.com>
2020-10-24 17:28:53 +0545

    minor update

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Why do i regret not knowing about GitHub Pages earlier?",
+        "excerpt":"Contents Introduction Create GitHub account Download and Install Ruby Make First Jekyll Project Deploy it on GitHub Make a Repository Blogging Before writing a Math Blog On _config.yml Show Formula First Blog Make Awesome Posts Add and View Comment Polish It Introduction A mathematics student from Nepal is about to...","categories": ["github-pages"],
+        "tags": [],
+        "url": "http://localhost:4000/2020/09/19/why-do-i-regret-not-knowing-about-gitHub-pages-earlier/",
+        "teaser": "http://localhost:4000/assets/teaser.png"
+      }]
