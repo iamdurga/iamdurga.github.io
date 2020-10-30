0bc2636f6d70e8b6cda385c15df7b5625366a5c9
iamdurga <pokhareldurga88@gmail.com>
2020-10-30 14:54:44 +0545

    minor change

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,7 +3,7 @@ var store = [{
         "excerpt":"Introduction I was teaching secondary level students on 2019 and I was learning lots of things during that 1 year. It was really an amazing experience that I could share lots of thing and at the same time I learned the best practices that can be done my teachers to...","categories": ["General"],
         "tags": ["thoughts","journal"],
         "url": "http://localhost:4000/2020/09/19/my-blogging-journey/",
-        "teaser": "http://localhost:4000/assets/images/what-if.png"
+        "teaser": "http://localhost:4000/assets/header_images/1.png"
       },{
         "title": "The Power of 'What if?'",
         "excerpt":"Introduction  What if I did this? What if I couldn’t do this?   \\[(a+b)^2 = a^2 + 2ab + b^2 \\\\ (a-b)^2 = a^2 - 2ab + b^2 \\\\ a^2-b^2 = (a+b)(a-b) \\\\\\] ","categories": ["General"],
@@ -17,63 +17,63 @@ var store = [{
         "url": "http://localhost:4000/2020/09/19/the-power-why-not/",
         "teaser": "http://localhost:4000/assets/images/head.png"
       },{
-        "title": "The Power of 'What if?'",
-        "excerpt":"Introduction  What if I did this? What if I couldn’t do this?  ","categories": ["General"],
+        "title": "What is Graph?",
+        "excerpt":"Comming soon….  ","categories": ["General"],
         "tags": ["thoughts","journal"],
         "url": "http://localhost:4000/2020/09/19/what-is-graph/",
         "teaser": "http://localhost:4000/assets/images/what-if.png"
       },{
         "title": "Does ax = bx imply a = b in any vector space?justify",
-        "excerpt":"Question: Does ax = bx imply a = b in any vector space? Justify. Let v = \\({(a_1, a_2):a_1, a_2 \\in f}\\) where f is field. Define addition of elements of v coordinate wise, and for \\(c \\in f\\) and \\((a_1, a_2) \\in V\\) . Define \\(c(a_1, a_2) = (a_1,...","categories": ["Linear","Algebra"],
+        "excerpt":"Question: Does ax = bx imply a = b in any vector space? Justify. Let v = \\({(a_1, a_2):a_1, a_2 \\in f}\\) where f is field. Define addition of elements of v coordinate wise, and for \\(c \\in f\\) and \\((a_1, a_2) \\in V\\) . Define \\(c(a_1, a_2) = (a_1,...","categories": ["Linear-Algebra"],
         "tags": ["Vectorspace","Basis"],
         "url": "http://localhost:4000/2020/10/23/Does-ax-=-bx-imply-a-=-b-in-any-vector-space/",
-        "teaser": "http://localhost:4000/assets/head.png"
+        "teaser": "http://localhost:4000/assets/header_images/6.png"
       },{
-        "title": "Is the empty set a subspace of every vector space ? Justify",
-        "excerpt":"Question:a. Is the empty set a subspace of every vector space ? Justify and let s = \\({(a, b, (a + b):a, b \\in R)}\\) . Is a subspace of \\(R^3\\) under the usual operation. b. Let I = (-a , a) a &gt; 0 be an open interval in...","categories": ["Linear","Algebra"],
+        "title": "Is the empty set a subspace of every vector space? Justify",
+        "excerpt":"Question:a. Is the empty set a subspace of every vector space ? Justify and let s = \\({(a, b, (a + b):a, b \\in R)}\\) . Is a subspace of \\(R^3\\) under the usual operation. b. Let I = (-a , a) a &gt; 0 be an open interval in...","categories": ["Linear-Algebra"],
         "tags": ["Vectorspace","Basis"],
         "url": "http://localhost:4000/2020/10/23/Is-the-empty-set-a-subspace-of-every-vector-space-Justify/",
-        "teaser": "http://localhost:4000/assets/head.png"
+        "teaser": "http://localhost:4000/assets/header_images/2.png"
       },{
         "title": "Is the span of X is X? Does every vector space have finite basis?",
-        "excerpt":"Question a: Is the span of \\(\\phi\\) is \\(\\phi\\) ? justify. The vectors \\(u_1 = ( 2, -3 , 1) , u_2 = (1 , 4 , -2) , u_3 = (-8 , 12 , -4) , u_4 = ( 1 , 37 , -17) , u_5 = (-3 ,...","categories": ["Linear","Algebra"],
+        "excerpt":"Question a: Is the span of \\(\\phi\\) is \\(\\phi\\) ? justify. The vectors \\(u_1 = ( 2, -3 , 1) , u_2 = (1 , 4 , -2) , u_3 = (-8 , 12 , -4) , u_4 = ( 1 , 37 , -17) , u_5 = (-3 ,...","categories": ["Linear-Algebra"],
         "tags": ["Vectorspace","Basis"],
         "url": "http://localhost:4000/2020/10/23/Is-the-span-of-x/",
-        "teaser": "http://localhost:4000/assets/images/what-if.png"
+        "teaser": "http://localhost:4000/assets/header_images/1.png"
       },{
         "title": "Is the zero vector a linear combination of any nonempty set of vectors?",
-        "excerpt":"Question: Is the zero vector a linear combination of any nonempty set of vectors ? Justify. Is the set of all differential real valued functions defined on R a subspace of c(R)? Justify your answer. Solution: First part: Yes,it is \\(\\vec{0} = 0.\\vec{v1} + 0.\\vec{v2} + ...+0.\\vec{vn}\\) Moreever, an empty...","categories": ["Linear","Algebra"],
+        "excerpt":"Question: Is the zero vector a linear combination of any nonempty set of vectors ? Justify. Is the set of all differential real valued functions defined on R a subspace of c(R)? Justify your answer. Solution: First part: Yes,it is \\(\\vec{0} = 0.\\vec{v1} + 0.\\vec{v2} + ...+0.\\vec{vn}\\) Moreever, an empty...","categories": ["Linear-Algebra"],
         "tags": ["Vectorspace","Basis"],
         "url": "http://localhost:4000/2020/10/23/Is-the-zero-vector-a-linear-combination-of-any-nonempty-set-of-vectors/",
-        "teaser": "http://localhost:4000/assets/images/what-if.png"
+        "teaser": "http://localhost:4000/assets/header_images/4.png"
       },{
-        "title": "May a vector space have more than one zero vector?justify",
-        "excerpt":"Question: May a vector space have more than one zero vector?justify. Let \\(v = {(a_1, a_2):a_1, a_2 \\in R}\\). for \\((a_1, a_2)\\) , \\((b_1, b_2)\\) = \\((a_1 + 2b_1, a_2 + 3b_2)\\) and \\(c(a_1, a_2)\\) = \\((ca_1, a_2)\\) .Is v a vector space over f with these operations? justify your...","categories": ["Linear","Algebra"],
+        "title": "May a vector space have more than one zero vector? Justify",
+        "excerpt":"Question: May a vector space have more than one zero vector?justify. Let \\(v = {(a_1, a_2):a_1, a_2 \\in R}\\). for \\((a_1, a_2)\\) , \\((b_1, b_2)\\) = \\((a_1 + 2b_1, a_2 + 3b_2)\\) and \\(c(a_1, a_2)\\) = \\((ca_1, a_2)\\) .Is v a vector space over f with these operations? justify your...","categories": ["Linear-Algebra"],
         "tags": ["Vectorspace","Basis"],
         "url": "http://localhost:4000/2020/10/23/May-a-vector-space-have-more-than-one-zero-vector-justify/",
         "teaser": "http://localhost:4000/assets/head.png"
       },{
-        "title": "Does the Vector space contain more more than one zero vector. ?'",
-        "excerpt":"Question: Does every vector space contain a zero vector? Justify. Let s = {0,1} and f = R in f(S, R). Show that f = g and f + g = h where f(t) = 2t + 1 , \\(g(t) = 1 + 4t - 2t^2\\) and h(t) = 5t...","categories": ["Linear","Algebra"],
+        "title": "Does the Vector space contain more more than one zero vector?",
+        "excerpt":"Question: Does every vector space contain a zero vector? Justify. Let s = {0,1} and f = R in f(S, R). Show that f = g and f + g = h where f(t) = 2t + 1 , \\(g(t) = 1 + 4t - 2t^2\\) and h(t) = 5t...","categories": ["Linear-Algebra"],
         "tags": ["Vectorspace","Basis"],
         "url": "http://localhost:4000/2020/10/23/does-the-vector-space-contain-more-than-one-zero-vector",
-        "teaser": "http://localhost:4000/assets/head.png"
+        "teaser": "http://localhost:4000/assets/header_images/1.png"
       },{
-        "title": "Is  The Trace of square matrix product of its diagonal element.?",
-        "excerpt":"Question: Is the trace of square matrice the product of its diagonal entries ? Justify. Determine whether the set \\(w = [ (a_1 , a_2 , a_3) \\in R_3 : a_1 + 2a_2 – 3a_3 = 2]\\) is subspace or not of \\(R^3\\) with justification.\\ Solution: First part: No, the...","categories": ["Linear","Algebra"],
+        "title": "Is  The Trace of square matrix product of its diagonal element?",
+        "excerpt":"Question: Is the trace of square matrice the product of its diagonal entries ? Justify. Determine whether the set \\(w = [ (a_1 , a_2 , a_3) \\in R_3 : a_1 + 2a_2 – 3a_3 = 2]\\) is subspace or not of \\(R^3\\) with justification.\\ Solution: First part: No, the...","categories": ["Linear-Algebra"],
         "tags": ["Vectorspace","Basis"],
         "url": "http://localhost:4000/2020/10/23/how-to-find-if-the-trace-of-square-matrix-product-of-diagonal-entries/",
-        "teaser": "http://localhost:4000/assets/images/what-if.png"
+        "teaser": "http://localhost:4000/assets/header_images/1.png"
       },{
         "title": "Example of row echelon form and reduce row echelon form of matrix.",
-        "excerpt":"Question: Find row echelon form and reduce row echelon form A = \\(\\left[\\begin{array}{cc} 0&amp;-1 &amp;2&amp;3&amp;4\\\\2&amp;1&amp;3&amp;4&amp;1\\\\1&amp;3&amp;-1&amp;2&amp;3\\\\3&amp;3&amp;-6&amp;4&amp;7 \\end{array}\\right]\\) Solution: Here given matrix is A = \\(\\left[\\begin{array}{cc} 0&amp;-1 &amp;2&amp;3&amp;4\\\\2&amp;1&amp;3&amp;4&amp;1\\\\1&amp;3&amp;-1&amp;2&amp;3\\\\3&amp;3&amp;-6&amp;4&amp;7 \\end{array}\\right]\\) \\[\\sim \\left[\\begin{array}{cc} 1&amp;3 &amp;-1&amp;2&amp;3\\\\2&amp;1&amp;3&amp;4&amp;1\\\\0&amp;-1&amp;2&amp;3&amp;4\\\\3&amp;3&amp;-6&amp;4&amp;7 \\end{array}\\right] \\ R_1 \\Leftrightarrow R_3\\] \\[\\sim \\left[\\begin{array}{cc} 1&amp;3 &amp;-1&amp;2&amp;3\\\\0&amp;-5&amp;5&amp;0&amp;-5\\\\0&amp;-1&amp;2&amp;3&amp;4\\\\0&amp;-6&amp;-3&amp;-2&amp;-2 \\end{array}\\right] \\ R_2 \\Leftarrow R_2 – 2R_1, R_4 \\Leftarrow R_4...","categories": ["Linear","Algebra"],
+        "excerpt":"Question: Find row echelon form and reduce row echelon form A = \\(\\left[\\begin{array}{cc} 0&amp;-1 &amp;2&amp;3&amp;4\\\\2&amp;1&amp;3&amp;4&amp;1\\\\1&amp;3&amp;-1&amp;2&amp;3\\\\3&amp;3&amp;-6&amp;4&amp;7 \\end{array}\\right]\\) Solution: Here given matrix is A = \\(\\left[\\begin{array}{cc} 0&amp;-1 &amp;2&amp;3&amp;4\\\\2&amp;1&amp;3&amp;4&amp;1\\\\1&amp;3&amp;-1&amp;2&amp;3\\\\3&amp;3&amp;-6&amp;4&amp;7 \\end{array}\\right]\\) \\[\\sim \\left[\\begin{array}{cc} 1&amp;3 &amp;-1&amp;2&amp;3\\\\2&amp;1&amp;3&amp;4&amp;1\\\\0&amp;-1&amp;2&amp;3&amp;4\\\\3&amp;3&amp;-6&amp;4&amp;7 \\end{array}\\right] \\ R_1 \\Leftrightarrow R_3\\] \\[\\sim \\left[\\begin{array}{cc} 1&amp;3 &amp;-1&amp;2&amp;3\\\\0&amp;-5&amp;5&amp;0&amp;-5\\\\0&amp;-1&amp;2&amp;3&amp;4\\\\0&amp;-6&amp;-3&amp;-2&amp;-2 \\end{array}\\right] \\ R_2 \\Leftarrow R_2 – 2R_1, R_4 \\Leftarrow R_4...","categories": ["Linear-Algebra"],
         "tags": ["Vectorspace","Basis"],
         "url": "http://localhost:4000/2020/10/23/row-echelon-and-reduce-echelon-form-of-matrix/",
-        "teaser": "http://localhost:4000/assets/images/what-if.png"
+        "teaser": "http://localhost:4000/assets/header_images/5.png"
       },{
         "title": "Zero Vector and its basis.",
-        "excerpt":"Question: Does the zero vector have no basis? Justify . Construct the real polynomial degree at most 2 whose graph contaions the point \\((-4 , 24) ,(1 , 9)\\ and\\ (3 , 3)\\) sketch the graph. Solution: First Part: No,empty set the basis for zero vector. Second part: Here \\(c_0...","categories": ["Linear","Algebra"],
+        "excerpt":"Question: Does the zero vector have no basis? Justify . Construct the real polynomial degree at most 2 whose graph contaions the point \\((-4 , 24) ,(1 , 9)\\ and\\ (3 , 3)\\) sketch the graph. Solution: First Part: No,empty set the basis for zero vector. Second part: Here \\(c_0...","categories": ["Linear-Algebra"],
         "tags": ["Vectorspace","Basis"],
         "url": "http://localhost:4000/2020/10/23/zero-vector-and-its-basis/",
-        "teaser": "http://localhost:4000/assets/images/what-if.png"
+        "teaser": "http://localhost:4000/assets/header_images/9.png"
       }]
