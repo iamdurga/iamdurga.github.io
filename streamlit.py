7b4d8e52bb29f31f27f6d8f73bd326e0c7c41d80
Durga Pokharel <pokhareldurga88@gmail.com>
2021-09-30 16:39:22 +0545

    New blog added

---
+++ streamlit.py
@@ -0,0 +1,18 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+"""
+# My first app
+Here's our first attempt at using data to create a table using streamlit.
+
+## I probabily can make mistake, it is my first app.
+"""
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
